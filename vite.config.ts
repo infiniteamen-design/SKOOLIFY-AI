672865464e0ebed301ae759c8d19e40564a41876
infiniteamen-design <infiniteamen@gmail.com>
2025-12-17 19:37:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+      // THIS LINE FIXES THE BLANK SCREEN
+      base: '/', 
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
